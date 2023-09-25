54118153b6de2fc6d6dfe4e9985183d39f348fd3
Mark <miniapple8888@gmail.com>
2023-09-25 02:39:35 -0400

    Animate Mario

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,17 +9,25 @@ public class PlayerMovement : MonoBehaviour
 {
     public JumpGoomba jumpGoomba;
     public float upSpeed = 10;
+    public float speed = 20;
+    public float deathImpulse = 5.0f;
     public TextMeshProUGUI scoreText;
     public Button resetButton;
     public GameObject overlay;
     public GameObject gameOverText;
     public GameObject enemies;
+    public Animator marioAnimator;
+    public AudioSource marioAudio;
+    public AudioClip marioDeath;
     private bool onGroundState = true;
-    private float speed = 10;
     private float maxSpeed = 20;
-    private bool facingRightState = true;
+    private bool faceRightState = true;
     private Rigidbody2D marioRG;
     private SpriteRenderer marioSprite;
+
+    // state
+    [System.NonSerialized]
+    public bool alive = true;
 
     // Start is called before the first frame update
     void Start()
@@ -27,25 +35,45 @@ public class PlayerMovement : MonoBehaviour
         Application.targetFrameRate = 10;
         marioRG = GetComponent<Rigidbody2D>();
         marioSprite = GetComponent<SpriteRenderer>();
+        marioAnimator.SetBool("onGround", onGroundState);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("a") && faceRightState) {
+            faceRightState = false;
+            marioSprite.flipX = true;
+            if (marioRG.velocity.x > 0.1f)
+                marioAnimator.SetTrigger("onSkid");
+        }
+        if (Input.GetKeyDown("d") && !faceRightState) {
+            faceRightState = true;
+            marioSprite.flipX = false;
+            if (marioRG.velocity.x < -0.1f)
+                marioAnimator.SetTrigger("onSkid");
+        }
+        marioAnimator.SetFloat("xSpeed", Mathf.Abs(marioRG.velocity.x));
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Enemy")) {
             Debug.Log("Collided with goomba!");
+            // play death animation
+            marioAnimator.Play("mario_die");
+            marioAudio.PlayOneShot(marioDeath);
+            alive = false;
             GameOver();
         }
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("Ground")) onGroundState = true;
+        if (col.gameObject.CompareTag("Ground")) {
+            onGroundState = true;
+            marioAnimator.SetBool("onGround", onGroundState);
+        }
     }
 
     // Fixed Update is called 50 times a second
@@ -53,33 +81,53 @@ public class PlayerMovement : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
         
-        if (Mathf.Abs(moveHorizontal) > 0) { // if player wants to move
-            Vector2 movement = new Vector2(moveHorizontal, 0);
-            // check it doesn't go beyond max speed
-            if (marioRG.velocity.magnitude < maxSpeed)
-                marioRG.AddForce(movement * speed);
-        }
+        if (alive) {
+            if (Mathf.Abs(moveHorizontal) > 0) { // if player wants to move
+                Vector2 movement = new Vector2(moveHorizontal, 0);
+                // check it doesn't go beyond max speed
+                if (marioRG.velocity.magnitude < maxSpeed)
+                    marioRG.AddForce(movement * speed);
+            }
 
-        // when player releases movement key, stop
-        if (Input.GetKeyUp("a") || Input.GetKeyUp("d")) {
-            marioRG.velocity = Vector2.zero;
-        }
+            // when player releases movement key, stop
+            if (Input.GetKeyUp("a") || Input.GetKeyUp("d")) {
+                marioRG.velocity = Vector2.zero;
+            }
 
-        if (Input.GetKeyDown("space") && onGroundState) {
-            marioRG.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
-            onGroundState = false;
-        }
+            if (Input.GetKeyDown("space") && onGroundState) {
+                marioRG.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
+                onGroundState = false;
+                marioAnimator.SetBool("onGround", onGroundState);
+            }
 
-        // toggle flipping mario when facing different direction
-        if (Input.GetKeyDown("a") && facingRightState) {
-            facingRightState = false;
-            marioSprite.flipX = true;
-        }
+            // toggle flipping mario when facing different direction
+            if (Input.GetKeyDown("a") && faceRightState) {
+                faceRightState = false;
+                marioSprite.flipX = true;
+            }
 
-        if (Input.GetKeyDown("d") && !facingRightState) {
-            facingRightState = true;
-            marioSprite.flipX = false;
+            if (Input.GetKeyDown("d") && !faceRightState) {
+                faceRightState = true;
+                marioSprite.flipX = false;
+            }   
         }
+        
+    }
+
+    void PlayJumpSound()
+    {
+        marioAudio.PlayOneShot(marioAudio.clip);
+    }
+
+    void PlayDeathImpulse()
+    {
+        marioRG.AddForce(Vector2.up * deathImpulse, ForceMode2D.Impulse);
+    }
+
+    void GameOverScene()
+    {
+        Time.timeScale = 0.0f;
+        GameOver();
     }
 
     public void RestartButtonCallback(int input)
@@ -92,13 +140,15 @@ public class PlayerMovement : MonoBehaviour
     private void ResetGame()
     {
         marioRG.transform.position = new Vector3(-6f, -3.23f, 0.0f);
-        facingRightState = true;
+        faceRightState = true;
         marioSprite.flipX = false;
         scoreText.text =  "Score: 0";
         foreach (Transform eachChild in enemies.transform) {
             eachChild.transform.localPosition = eachChild.GetComponent<EnemyMovement>().startPosition;
         }
         jumpGoomba.score = 0;
+        marioAnimator.SetTrigger("gameRestart");
+        alive = true;
         scoreText.transform.localPosition = new Vector3(-842.0f, 488.0f, 0.0f);
         resetButton.transform.localPosition = new Vector3(898.0f, 481.0f, 0.0f);
         overlay.SetActive(false);
